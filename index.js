c512405b42a75ba7c1222734e06a104e0ba358df
Harikesh Kushwaha <harikeshkumar0926@gmail.com>
2022-06-29 19:29:32 +0530

    use ejs f for homepage

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const mongoose = require("mongoose");
+// const methodOverride = require("method-override");
 
+const Book = require("./models/book");
+
+mongoose
+  .connect("mongodb://localhost:27017/bookGallery", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MONGO CONNECTION OPEN!!!");
+  })
+  .catch((err) => {
+    console.log("OH NO MONGO CONNECTION ERROR!!!!");
+    console.log(err);
+  });
+
+app.use(express.urlencoded({ extended: true }));
+// app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "statics")));
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "/views"));
+app.set("views", path.join(__dirname, "views"));
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   title = "Welcome to the Homepage";
-  res.render("home", { title: title });
+  const books = await Book.find();
+  res.render("home", { title: title, books: books });
 });
 
 app.listen(3000, () => {
